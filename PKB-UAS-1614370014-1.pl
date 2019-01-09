a3629a918b0d3f8a07cd4b8801fecec2a813ef27
muhammadseftian <45618357+muhammadseftian@users.noreply.github.com>
2019-01-09 19:59:00 +0700

    Add files via upload

---
+++ PKB-UAS-1614370014-1.pl
@@ -0,0 +1,13 @@
+putra(susilo, jodi).
+putra(susilo, ferdinan).
+putra(jodi, suwardi).
+putra(ferdinan, widodo).
+putra(kandi, wisnu).
+putra(suwardi, boni).
+putra(mirna, dani).
+putra(cindy, arianto).
+
+
+
+calon direktur(susilo, jodi).
+calon direktur(susilo, ferdinan).
